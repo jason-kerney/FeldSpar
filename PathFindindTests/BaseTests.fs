78765477c44a2394d4c5dfd83909905e2811ecaa
Jason Kerney <jason.kerney@gmail.com>
2014-07-21 13:23:22 -0700

    Added Comments explaining test scenario

--- PathFindindTests/BaseTests.fs
+++ PathFindindTests/BaseTests.fs
@@ -4,3 +4,12 @@ module BaseTests =
     type internal Marker = interface end
     let testPathFindingAssembly = typeof<Marker>.Assembly
 
+    // Given a 3 x 4 rectangulare graph
+    //  a stating point of 0, 1
+    //  a ending point of 3, 1
+    //  and no obstructions
+    // then the path should be
+    //  0,1
+    //  1,1
+    //  2,1
+    //  3,1
