e2ff72794772868bde7eb08dbadee8c3798ff36a
Jason Kerney <jason.kerney@gmail.com>
2014-07-21 07:53:36 -0700

    Removed unnecessary let bindings

--- FeldSparFramework/TestSummaryUtilities.fs
+++ FeldSparFramework/TestSummaryUtilities.fs
@@ -17,32 +17,26 @@ module TestSummaryUtilities =
 
     let JSONFormat (value:OutputReport) =
         let successes = 
-            let subSuccesses =
-                value.Successes
-                |> Seq.map(fun s -> sprintf "\"%s\"" (s.Replace("\"", "'")))
-                |> joinWith ",\r\n\t\t"
-
-            subSuccesses
+            value.Successes
+            |> Seq.map(fun s -> sprintf "\"%s\"" (s.Replace("\"", "'")))
+            |> joinWith ",\r\n\t\t"
 
         let failures = 
-            let subFailures = 
-                value.Failures
-                |> Array.map (fun { Name = name; FailureType = failureType } ->
-                    let failMsg = 
-                        match failureType with
-                        | GeneralFailure(msg)     -> sprintf "General Failure ('%s')" msg
-                        | ExceptionFailure(ex)    -> sprintf "Exception Thrown:\r\n%A" ex
-                        | ExpectationFailure(msg) -> sprintf "Expectation Not Met ('%s')" msg
-                        | Ignored(msg)            -> sprintf "Ignored ('%s')" msg
-                        | StandardNotMet          -> "Standard was not Met"
+            value.Failures
+            |> Array.map (fun { Name = name; FailureType = failureType } ->
+                let failMsg = 
+                    match failureType with
+                    | GeneralFailure(msg)     -> sprintf "General Failure ('%s')" msg
+                    | ExceptionFailure(ex)    -> sprintf "Exception Thrown:\r\n%A" ex
+                    | ExpectationFailure(msg) -> sprintf "Expectation Not Met ('%s')" msg
+                    | Ignored(msg)            -> sprintf "Ignored ('%s')" msg
+                    | StandardNotMet          -> "Standard was not Met"
 
-                    let failMsg = failMsg.Replace("\"", "'")
+                let failMsg = failMsg.Replace("\"", "'")
 
-                    sprintf "{ \"%s\" : \"%s\" }" name failMsg
-                )
-                |> joinWith ",\r\n\t\t"
-
-            subFailures
+                sprintf "{ \"%s\" : \"%s\" }" name failMsg
+            )
+            |> joinWith ",\r\n\t\t"
 
         let jsonString = sprintf "{\r\n\t\"Failures\" : [\r\n\t\t%s\r\n\t],\r\n\t\"Successes\" : [\r\n\t\t%s\r\n\t]\r\n}\r\n"  failures successes
         jsonString
