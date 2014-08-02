caeb82009d7b05752f3136d1a0f7a8ac31169850
Jason Kerney <jason.kerney@gmail.com>
2014-08-02 13:40:10 -0400

    Added a Grid Type

---
+++ PathFindindTests/Framework.fs
@@ -0,0 +1,13 @@
+﻿namespace PathFinding
+
+type Grid = 
+    {
+        Up : Grid Option;
+        Right : Grid Option;
+        Down : Grid Option;
+        Left : Grid Option;
+    }
+
+module Framework =
+    ()
+
