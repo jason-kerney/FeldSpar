c836959fdfcc367b0d1e0c9772bbe8823e4a9288
Jason Kerney <jason.kerney@gmail.com>
2014-11-11 15:17:28 -0800

    Making things a bit nicer to look at

---
+++ GuiRunner/ValueConverters/TestStatusToFontWeightConverter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Windows.Data;
+using GuiRunner.StyleConstants;
+using ViewModel;
+
+namespace GuiRunner.ValueConverters
+{
+    public class TestStatusToFontWeightConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var status = (TestStatus) value;
+
+            switch (status)
+            {
+                case TestStatus.Failure:
+                    return TestStatusFontWeight.FailedFontWeight;
+                case TestStatus.Ignored:
+                    return TestStatusFontWeight.IgnoredFontWeight;
+                case TestStatus.Running:
+                    return TestStatusFontWeight.RunningFontWeight;
+                case TestStatus.Success:
+                    return TestStatusFontWeight.SuccessFontWeight;
+                default:
+                    return TestStatusFontWeight.NoneFontWeight;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
