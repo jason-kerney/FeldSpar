0990040805d0087c901f14a8a8dcf789b1f71fc9
Jason Kerney <jason.kerney@gmail.com>
2014-11-13 12:59:20 -0800

    Updated Versions

--- FeldSparFramework/AssemblyInfo.fs
+++ FeldSparFramework/AssemblyInfo.fs
@@ -1,6 +1,6 @@
 ﻿module AssemblyInfo
 open System.Reflection
-[<assembly: AssemblyVersion("0.2.2")>]
-[<assembly: AssemblyFileVersion("0.2.2")>]
+[<assembly: AssemblyVersion("0.2.3")>]
+[<assembly: AssemblyFileVersion("0.2.3")>]
 [<assembly: AssemblyProduct("FeldSpar F# Unit testing framework")>]
 ()
