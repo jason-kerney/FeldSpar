b009f8bb446897fcafc6fbf17f5f23c711a5964f
Jason Kerney <jason.kerney@gmail.com>
2014-11-26 19:50:07 -0800

    Changing the project that nuget uses

--- FeldSpar.Console/AssemblyInfo.fs
+++ FeldSpar.Console/AssemblyInfo.fs
@@ -1,6 +1,6 @@
 ﻿module AssemblyInfo
 open System.Reflection
-[<assembly: AssemblyVersion("0.2.0.2")>]
-[<assembly: AssemblyFileVersion("0.2.0.2")>]
+[<assembly: AssemblyVersion("0.2.1.0")>]
+[<assembly: AssemblyFileVersion("0.2.1.0")>]
 [<assembly: AssemblyProduct("Console runner for FeldSpar F# Unit testing framework")>]
 ()
--- FeldSparFramework/AssemblyInfo.fs
+++ FeldSparFramework/AssemblyInfo.fs
@@ -1,6 +1,6 @@
 ﻿module AssemblyInfo
 open System.Reflection
-[<assembly: AssemblyVersion("0.4.0.2")>]
-[<assembly: AssemblyFileVersion("0.4.0.2")>]
+[<assembly: AssemblyVersion("0.5.0.0")>]
+[<assembly: AssemblyFileVersion("0.5.0.0")>]
 [<assembly: AssemblyProduct("FeldSpar F# Unit testing framework")>]
 ()
