995d668c3b8fc4eb8b301fd7b172e183ddd16c02
Jason Kerney <jason.kerney@gmail.com>
2014-11-27 11:21:58 -0800

    Removed old runner

--- FeldSparFramework/Framework.fs
+++ FeldSparFramework/Framework.fs
@@ -20,12 +20,18 @@ type ExecutionSummary =
         TestResults : TestResult;
     }
 
+/// <summary>
+/// Data about a failing test
+/// </summary>
 type FailureReport = 
     {
         Name : string;
         FailureType : FailureType;
     }
 
+/// <summary>
+/// Data about a test assembly results
+/// </summary>
 type OutputReport =
     {
         Name : string;
@@ -33,11 +39,17 @@ type OutputReport =
         Successes : string[];
     }
     
+/// <summary>
+/// A type used to configure reporters for gold standard (approval tests)
+/// </summary>
 type AssemblyConfiguration =
     {
         Reporters : (unit -> IApprovalFailureReporter) List;
     }
 
+/// <summary>
+/// Information about the current executing test evironment
+/// </summary>
 type TestEnvironment =
     {
         Name:string;
@@ -48,32 +60,51 @@ type TestEnvironment =
         Reporters : (unit -> IApprovalFailureReporter) List;
     }
 
+/// <summary>
+/// A type represeting a unit test
+/// </summary>
 type Test = | Test of (TestEnvironment -> TestResult)
-type IgnoredTest = | ITest of (TestEnvironment -> TestResult)
-type Configuration = | Config of (unit -> AssemblyConfiguration)
-type RunConfiguration = 
-    {
-        Assembly: Reflection.Assembly;
-        Config: Configuration option;
-    }
 
+/// <summary>
+/// A type representing an ignored unit test
+/// </summary>
+type IgnoredTest = | ITest of (TestEnvironment -> TestResult)
+
+/// <summary>
+/// A type to allow the dynamic loading of configuration if it is used
+/// </summary>
+type Configuration = | Config of (unit -> AssemblyConfiguration)
+
+/// <summary>
+/// Data representing a therory or parameterized test.
+/// </summary>
 type TheoryCaseTemplate<'a> =
     {
         UnitDescription : 'a -> string;
         UnitTest : 'a -> TestEnvironment -> TestResult;
     }
     
+/// <summary>
+/// Data that shows how to combine a theory test with data
+/// </summary>
 type TestTheoryTemplate<'a> =
     {
         Data : seq<'a>;
         Base : TheoryCaseTemplate<'a>;
     }
 
+/// <summary>
+/// A type used to creat a theory test case
+/// </summary>
 type Theory<'a> =
     | Theory of TestTheoryTemplate<'a>
 
 [<AutoOpen>]
 module Utilities =
+    /// <summary>
+    /// Gets the failure message from a result or an empty string if success
+    /// </summary>
+    /// <param name="result">Dhe result from which to get the message.</param>
     let getMessage result = 
         match result with
         | Success -> String.Empty
@@ -83,6 +114,11 @@ module Utilities =
         | Failure(ExpectationFailure(m)) -> m
         | Failure(Ignored(m)) -> m
 
+    /// <summary>
+    /// Adds an additional comment to the beginnig of a failure message
+    /// </summary>
+    /// <param name="comment">The comment that is added</param>
+    /// <param name="result">The result to add the comment to. The comment is only added if the result is a failure</param>
     let withFailComment comment result = 
         match result with
         | Success | Failure(StandardNotMet(_)) | Failure(ExceptionFailure(_)) -> result
@@ -99,25 +135,58 @@ module Utilities =
             Failure(fail)
             
 
+    /// <summary>
+    /// Returns a Failure result with failure type of 'Ignored' and with the desired message
+    /// </summary>
+    /// <param name="message"></param>
     let ignoreWith message = Failure(Ignored(message))
 
+    /// <summary>
+    /// Wraps an exception into a failure result with a failure type of 'FailureException'
+    /// </summary>
+    /// <param name="ex">The exception to convert to a failure result</param>
     let failException ex = Failure(ExceptionFailure(ex))
 
+    /// <summary>
+    /// Creates a failure result of type 'GeneralFailure' with a desired message
+    /// </summary>
+    /// <param name="message">The message to wrap into a failure result</param>
     let failResult message = Failure(GeneralFailure(message))
 
+    /// <summary>
+    /// Returns a failure result of failure type 'Ignored' with a message indicating test is not yet implemented
+    /// </summary>
     let ``Not Yet Implemented`` = ignoreWith "Test not yet implemented"
 
+
+    /// <summary>
+    /// Returns a failure result of failure type 'Ignored' with a message indicating test has an indeterminate test result.
+    /// </summary>
     let indeterminateTest = ignoreWith "Indeterminate Test Result"
 
+    /// <summary>
+    /// An alias for Seq.map
+    /// </summary>
+    /// <param name="items">the items to map</param>
+    /// <param name="call">the function that is called on each item</param>
     let calledWithEachOfThese (items: 'a seq) call =
-        seq { for i in items do
-                yield call i }
-            
+        items |> Seq.map call
+      
+    /// <summary>
+    /// Maps all items using each call function and returns sequence of all permutations
+    /// </summary>
+    /// <param name="items">the items to map</param>
+    /// <param name="calls">the mapping functions that will get called on each item</param>
     let andAlsoEachOfThese items (calls: ('a -> 'b) seq) =
         seq { for callWith in calls do
                  for item in items do 
                     yield callWith item }
 
+    /// <summary>
+    /// Takes results and divides them up by success and failures
+    /// </summary>
+    /// <param name="name">The name of the test assembly</param>
+    /// <param name="results">the test results</param>
     let buildOutputReport (name, results:ExecutionSummary seq) =
         let successes = 
             results
