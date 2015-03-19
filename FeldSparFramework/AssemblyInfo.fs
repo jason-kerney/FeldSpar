b39ec66dddacacd1f98b28c04dc32b717e188524
Jason Kerney <Jason@TeamKerney.com>
2015-03-19 11:49:18 -0700

    Reved Version on  Framework to represent the change to JSON report sorting.

--- FeldSparFramework/AssemblyInfo.fs
+++ FeldSparFramework/AssemblyInfo.fs
@@ -2,8 +2,8 @@
 open System.Reflection
 open System.Resources
 
-[<assembly: AssemblyVersion("0.5.1.0")>]
-[<assembly: AssemblyFileVersion("0.5.1.0")>]
+[<assembly: AssemblyVersion("0.5.1.1")>]
+[<assembly: AssemblyFileVersion("0.5.1.1")>]
 [<assembly: AssemblyProduct("FeldSpar F# Unit testing framework")>]
 [<assembly: NeutralResourcesLanguageAttribute("en")>]
 ()
