2550e0f699770abc487926cc9dea065d7858d34c
Jason Kerney <Jason@TeamKerney.com>
2015-09-26 04:36:23 -0700

    Removed GUI based runner and Tests that had nothing to do with project

--- FSharpWpf/AssemblyInfo.fs
+++
@@ -1,9 +0,0 @@
-﻿module AssemblyInfo
-open System.Reflection
-open System.Resources
-
-[<assembly: AssemblyVersion("0.3.1.0")>]
-[<assembly: AssemblyFileVersion("0.3.1.0")>]
-[<assembly: AssemblyProduct("GUI runner for FeldSpar F# Unit testing framework")>]
-[<assembly: NeutralResourcesLanguageAttribute("en")>]
-()
