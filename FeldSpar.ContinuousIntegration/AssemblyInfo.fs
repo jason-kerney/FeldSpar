cf8103698af30b721f32019738d013402c924a19
Jason Kerney <Jason@TeamKerney.com>
2015-09-28 20:06:14 -0700

    Updated version to release with new Approvals

--- FeldSpar.ContinuousIntegration/AssemblyInfo.fs
+++ FeldSpar.ContinuousIntegration/AssemblyInfo.fs
@@ -2,8 +2,8 @@
 open System.Reflection
 open System.Resources
 
-[<assembly: AssemblyVersion("0.3.0.1")>]
-[<assembly: AssemblyFileVersion("0.3.0.1")>]
+[<assembly: AssemblyVersion("0.3.0.2")>]
+[<assembly: AssemblyFileVersion("0.3.0.2")>]
 [<assembly: AssemblyProduct("Console runner for FeldSpar F# Unit testing framework")>]
 [<assembly: NeutralResourcesLanguageAttribute("en")>]
 ()
--- FeldSparFramework/AssemblyInfo.fs
+++ FeldSparFramework/AssemblyInfo.fs
@@ -2,8 +2,8 @@
 open System.Reflection
 open System.Resources
 
-[<assembly: AssemblyVersion("0.6.0.0")>]
-[<assembly: AssemblyFileVersion("0.6.0.0")>]
+[<assembly: AssemblyVersion("0.6.0.1")>]
+[<assembly: AssemblyFileVersion("0.6.0.1")>]
 [<assembly: AssemblyProduct("FeldSpar F# Unit testing framework")>]
 [<assembly: NeutralResourcesLanguageAttribute("en")>]
 ()
