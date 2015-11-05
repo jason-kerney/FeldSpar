e55117eb6cc49037a027978409aff576aab7a8b9
Jason Kerney <Jason@TeamKerney.com>
2015-11-05 07:33:39 -0800

    Fixed F# Version

--- FeldSpar.ContinuousIntegration/AssemblyInfo.fs
+++ FeldSpar.ContinuousIntegration/AssemblyInfo.fs
@@ -2,8 +2,8 @@
 open System.Reflection
 open System.Resources
 
-[<assembly: AssemblyVersion("1.0.0.0")>]
-[<assembly: AssemblyFileVersion("1.0.0.0")>]
+[<assembly: AssemblyVersion("1.1.0.0")>]
+[<assembly: AssemblyFileVersion("1.1.0.0")>]
 [<assembly: AssemblyProduct("Console runner for FeldSpar F# Unit testing framework")>]
 [<assembly: NeutralResourcesLanguageAttribute("en")>]
 ()
