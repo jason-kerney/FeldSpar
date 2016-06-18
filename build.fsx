09199eaf5212ddb55aa8ee560f58aae8b9ab0250
Jason Kerney <Jason@TeamKerney.com>
2016-06-18 12:40:59 -0700

    removed hardcoded framework version

--- build.fsx
+++ build.fsx
@@ -8,11 +8,15 @@ RestorePackages ()
 // Properties
 let net46 = "46"
 let net40 = "40"
-let netVersion = net46
-let netDir = Some(netVersion)
-let netVersionFileName = "." + netVersion
-let fSharpProjects = "*" + netVersionFileName + ".*.fsproj"
-let releaseDir = "bin/Release/"
+
+let netVersionFileName = function
+    | None -> ""
+    | Some(v) -> "." + v
+
+let fSharpProjects = function
+    | None -> ""
+    | Some(netVersionFileName) -> "*" + netVersionFileName + ".*.fsproj"
+
 
 let getSubFolder = function
     | None -> ""
@@ -22,17 +26,26 @@ let getFolder root nv =
     let subFolder = getSubFolder nv
     root + subFolder
 
+let releaseDir netDir = 
+    let baseDir = "bin/Release"
+    match netDir with
+    | None -> baseDir + "/"
+    | Some(ver) -> baseDir +  ver + "/"
+
 let buildDir = getFolder "./_build/"
 let testDir = getFolder "./_test/"
 let deployDir = getFolder "./_deploy/"
 
-let nugetDeployDir = 
+let nugetDeployDir netDir = 
     let t = "C:/Nuget.Local/"
     if System.IO.Directory.Exists t then t
     else netDir |> deployDir
 
-let version () =
-    (netDir |> buildDir) + "FeldSparFramework" + netVersionFileName + ".dll" |> GetAssemblyVersionString 
+let feldSpar = "FeldSparFramework"
+let continuousIntegration = "ContinuousIntegration"
+
+let version netDir =
+    (netDir |> buildDir) + feldSpar + (netVersionFileName netDir) + ".dll" |> GetAssemblyVersionString 
 
 let build appDir tmpDir targetDir label projecType =
     let tmpDir = (appDir + tmpDir)
@@ -49,13 +62,54 @@ let build appDir tmpDir targetDir label projecType =
         |> Copy targetDir
     ()
 
-// Default target
-Target "Clean" (fun _ ->
-    CleanDirs [None |> buildDir; None |> testDir; None |> deployDir; nugetDeployDir]
-)
+let asPath baseName dirName =
+    baseName + "/" + dirName + "/"
 
 let buildApp netDir = 
-    build "./FeldSparFramework/" releaseDir (buildDir netDir) "AppBuild-Output:" fSharpProjects
+    let target = feldSpar |> asPath "."
+    let destination = feldSpar |> asPath (buildDir netDir)
+    let projects = fSharpProjects netDir
+    let release = releaseDir netDir
+    build target release destination "AppBuild-Output:" projects
+
+
+let buildConsole netDir =
+    let projects = fSharpProjects netDir
+    build ("./FeldSpar." + continuousIntegration + "/") (releaseDir netDir) (buildDir netDir) "BuildConsole-Output:" projects
+
+let buildTest netDir =
+    let dir = netDir |> testDir
+    let projects = fSharpProjects netDir
+    !!("./**/" + projects)
+        |> MSBuildRelease dir "Build"
+        |> Log "TestBuild-Output:"
+
+let tee f (g) (a:'a) =
+    f a |> g
+
+let test netDir = 
+    let a = FileSystemHelper.directoryInfo "./FeldSpar.Tests/"
+    
+    let b = a |> FileSystemHelper.filesInDir
+
+    let c = b  |> Array.map(fun fi -> fi.FullName)
+
+    let d = c |> Array.filter(fun fi -> fi.Contains("approved"))
+    
+    d |> Copy (testDir netDir)
+
+    let netVersion = netVersionFileName netDir
+
+    let result = Shell.Exec ((buildDir netDir) + "FeldSpar" + netVersion + "." + continuousIntegration + ".exe" ,"--v ERRORS --r \".\\RunReport.json\"  --a \".\\FeldSpar" + netVersion + ".Tests.exe\"", ?dir=Some(testDir netDir))
+    if result <> 0 then failwith "Failed Tests"
+
+Target "Nuke" (fun _ ->
+    Shell.Exec ("git", "clean -xfd")  |> ignore
+)
+
+Target "Clean" (fun _ ->
+    CleanDirs [None |> buildDir; None |> testDir; None |> deployDir; nugetDeployDir None]
+)
 
 let doesNothing = (fun _ -> ())
 
@@ -69,9 +123,6 @@ Target "BuildApp40" (fun _ ->
     Some(net40) |> buildApp
 )
 
-let buildConsole netDir =
-    build "./FeldSpar.ContinuousIntegration/" releaseDir (buildDir netDir) "BuildConsole-Output:" fSharpProjects
-
 Target "BuildConsole" doesNothing
 
 Target "BuildConsole40" (fun _ ->
@@ -81,12 +132,6 @@ Target "BuildConsole40" (fun _ ->
 Target "BuildConsole46" (fun _ ->
     Some(net46) |> buildConsole
 )
-
-let buildTest netDir =
-    let dir = netDir |> testDir
-    !! ("./**/" + fSharpProjects)
-        |> MSBuildRelease dir "Build"
-        |> Log "TestBuild-Output:"
 
 Target "BuildTest" doesNothing
          
@@ -98,15 +143,6 @@ Target "BuildTest46" (fun _ ->
     Some(net46) |> buildTest
 )
 
-let test netDir = 
-    FileSystemHelper.directoryInfo "./FeldSpar.Tests/" |>
-        FileSystemHelper.filesInDir |>
-        Array.map(fun fi -> fi.FullName) |>
-        Array.filter(fun fi -> fi.Contains("approved")) |>
-        Copy (testDir netDir)
-    let result = Shell.Exec ((buildDir netDir) + "FeldSpar" + netVersionFileName + ".ContinuousIntegration.exe" ,"--v ERRORS --r \".\\RunReport.json\"  --a \".\\FeldSpar" + netVersionFileName + ".Tests.exe\"", ?dir=Some(testDir netDir))
-    if result <> 0 then failwith "Failed Tests"
-
 Target "Test" doesNothing
 
 Target "Test40" (fun _ ->
@@ -117,7 +153,7 @@ Target "Test46" (fun _ ->
     Some(net46) |> test
 )
 
-Target "Zip" (fun _ ->
+let zip netDir =
     let sourceDir = None |> buildDir
     let destDir = None |> deployDir
     
@@ -126,29 +162,33 @@ Target "Zip" (fun _ ->
         dirInfo.Create()
 
     !! (sourceDir + "/**/*.*")
-        |> Zip sourceDir (destDir + "FeldSparFSharp." + (version ()) + ".zip")
+        |> Zip sourceDir (destDir + "FeldSparFSharp." + (version netDir) + ".zip")
+
+Target "Zip" (fun _ ->
+    zip (Some(net40))
+    zip (Some(net46))
 )
 
-Target "Default" (fun _ ->
-    trace "Hello world from FAKE"
-)
+Target "Default" doesNothing
 
 Target "Nuget" (fun _ ->
-    Shell.Exec ("nuget", @"pack ..\FeldSparFramework\FeldSpar" + netVersionFileName + ".Framework.fsproj -IncludeReferencedProjects -Prop Configuration=Release", netDir |> deployDir) |> ignore
-    Shell.Exec ("nuget", @"pack ..\FeldSpar.ContinuousIntegration\FeldSpar" + netVersionFileName + ".ContinuousIntegration.fsproj -IncludeReferencedProjects -Prop Configuration=Release", netDir |> deployDir) |> ignore
+    //Shell.Exec ("nuget", @"pack ..\FeldSparFramework\FeldSpar" + (netVersionFileName netDir) + ".Framework.fsproj -IncludeReferencedProjects -Prop Configuration=Release", netDir |> deployDir) |> ignore
+    //Shell.Exec ("nuget", @"pack ..\FeldSpar.ContinuousIntegration\FeldSpar" + (netVersionFileName netDir) + ".ContinuousIntegration.fsproj -IncludeReferencedProjects -Prop Configuration=Release", netDir |> deployDir) |> ignore
+    ()
 )
 
-Target "LocalDeploy" (fun _ ->
+let localDelpoy netDir = 
     let dir = netDir |> deployDir
-    if dir = nugetDeployDir then ()
+    let nugetDir = nugetDeployDir netDir
+    if dir = nugetDir then ()
     else
         FileSystemHelper.directoryInfo dir
             |> FileSystemHelper.filesInDir
             |> Array.filter (fun fi -> fi.Extension = ".nupkg")
             |> Array.map (fun fi -> fi.FullName)
-            |> Copy nugetDeployDir
+            |> Copy nugetDir
 
-        FileSystemHelper.directoryInfo nugetDeployDir
+        FileSystemHelper.directoryInfo nugetDir
             |> FileSystemHelper.filesInDir
             |> Array.map (fun fi -> fi.FullName)
             |> Array.iter (printfn "LocalDeploy-Output: %s")
@@ -156,7 +196,7 @@ Target "LocalDeploy" (fun _ ->
     use file = System.IO.File.Create(dir + "push.txt")
     let writer = new System.IO.StreamWriter(file)
 
-    FileSystemHelper.directoryInfo nugetDeployDir
+    FileSystemHelper.directoryInfo nugetDir
         |> FileSystemHelper.filesInDir
         |> Array.filter (fun fi -> fi.Extension = ".nupkg")
         |> Array.map (fun fi -> fi.FullName)
@@ -165,7 +205,12 @@ Target "LocalDeploy" (fun _ ->
     writer.Close()
 
     printfn "%A" (System.DateTime.Now)
+
+Target "LocalDeploy" (fun _ ->
+    localDelpoy (Some(net40))
+    localDelpoy (Some(net46))
 )
+
 
 // Dependencies
 "Clean"
@@ -187,6 +232,7 @@ Target "LocalDeploy" (fun _ ->
 
     ==> "Zip"
     ==> "Default"
+
     ==> "Nuget"
     ==> "LocalDeploy"
 
