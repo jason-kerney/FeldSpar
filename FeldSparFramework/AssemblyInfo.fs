17e1a49506cce35178b035bdcde4585bc66479f5
Jason Kerney <Jason@TeamKerney.com>
2016-08-20 14:36:07 -0700

    Updated version for push to Nuget

--- FeldSparFramework/AssemblyInfo.fs
+++ FeldSparFramework/AssemblyInfo.fs
@@ -2,8 +2,8 @@
 open System.Reflection
 open System.Resources
 
-[<assembly: AssemblyVersion("1.2.0.0")>]
-[<assembly: AssemblyFileVersion("1.2.0.0")>]
+[<assembly: AssemblyVersion("1.2.0.1")>]
+[<assembly: AssemblyFileVersion("1.2.0.1")>]
 [<assembly: AssemblyProduct("FeldSpar F# Unit testing framework")>]
 [<assembly: NeutralResourcesLanguageAttribute("en")>]
 ()
